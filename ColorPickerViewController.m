eb1025527959347344049cebc21df2ec9e70afd5
Fabian Canas <fcanas@gmail.com>
2012-10-07 20:42:06 -0600

    Remove last trace of preprocessor switch on color-saving logic.

--- ColorPickerViewController.m
+++ ColorPickerViewController.m
@@ -86,12 +86,7 @@ NSString *keyForBright = @"bright";
 
 - (void)dealloc {
     // Release claimed resources also
-#ifdef IPHONE_COLOR_PICKER_SAVE_DEFAULT
     [defaultsKey release];
-#else
-    [defaultsColor release];
-#endif
-
     [super dealloc];
 }
 
